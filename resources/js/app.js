897ecb793f98bb7632d6829fe15ff9fe7985010d
nasu-nt <nh33kzy@yahoo.co.jp>
2026-03-02 15:26:31 +0900

    ログイン画面作成(画面のみ)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+import './pages/login';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/views/components/header-app.blade.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <h1 class="title">HOUSEHOLD BUDGET</h1>
+    
+    <div>
+        <div>
+            Insights
+        </div>     
+        <div>
+            Settings
+        </div>
+    </div>
+</header>
